1144ed301e946390b71aecb638db59d35a3f29e7
Wahaj <wahaj1020@gmail.com>
2023-10-24 20:06:04 +0500

    merged-code-dark-theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,15 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   mode: "jit",
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
+        basic: "#0062ff",
         trueGray: colors.neutral,
       },
     },
